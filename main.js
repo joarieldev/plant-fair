54f7b5069c5b7e0a9a1b5452c77da92b372dd73e
Jose Martinez <joariel17@gmail.com>
2024-10-30 00:22:17 -0300

    add: animation

--- main.js
+++ main.js
@@ -73,3 +73,30 @@ function activeLink() {
     }
   })
 }
+
+
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '25px',
+  duration: 1500,
+  delay: 200,
+})
+
+sr.reveal('#inicio-left')
+sr.reveal('#inicio-right', {delay: 400, scale: 0.5})
+
+sr.reveal('.sub-title, .sub-title-paragraph', {delay: 100, distance: '0px'})
+
+sr.reveal('[data-name="event-card"]', {interval: 100})
+
+sr.reveal('#leaf', {origin: 'left'})
+sr.reveal('#seeding', {origin: 'right'})
+
+sr.reveal('#form-inscripciones', {delay: 100, distance: '0px', easing: 'ease-out'})
+sr.reveal('#plant-1', {delay: 300, origin: 'top', distance: '50px', reset: true})
+sr.reveal('#plant-2', {delay: 300, origin: 'bottom', distance: '60px', reset: true})
+
+sr.reveal('#footer-left', {origin: 'top'})
+sr.reveal('#footer-right', {delay: 400, origin: 'top'})
+sr.reveal('#footer-blob', {delay: 700, origin: 'bottom', distance: '60px'})
+sr.reveal('#footer-end', {origin: 'bottom', distance: '50px'})
