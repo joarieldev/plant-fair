eb28c28941716064b6545d9dcafcd56a6ae3e1ea
Jose Martinez <joariel17@gmail.com>
2024-11-01 21:04:03 -0300

    add: light and dark mode

--- main.js
+++ main.js
@@ -58,6 +58,7 @@ async function getClima() {
 
 getClima()
 
+// animar los links de navegacion
 window.addEventListener('scroll', activeLink)
 
 function activeLink() {
@@ -76,7 +77,7 @@ function activeLink() {
   })
 }
 
-
+// animar los elementos html
 const sr = ScrollReveal({
   origin: 'top',
   distance: '25px',
@@ -113,3 +114,51 @@ $form.addEventListener('submit', (e) => {
     $inputEmail.value = ''
   }, 6500)
 })
+
+// cambio a modo dark y light
+const $btnTheme = $('#btnTheme')
+const $sun = $('#sun')
+const $moon = $('#moon')
+
+$btnTheme.addEventListener('click', () => {
+  const theme = getTheme()
+  if (!document.startViewTransition){
+    if (theme === 'dark') updateTheme('light')
+    if (theme === 'light') updateTheme('dark') 
+    return
+  }
+
+  if (theme === 'dark')
+    document.startViewTransition(() => updateTheme('light'))
+  if (theme === 'light') 
+    document.startViewTransition(() => updateTheme('dark'))
+})
+
+const getTheme = () => {
+  const themeStorage = localStorage.getItem('theme')
+  if ( themeStorage !== 'undefined' && themeStorage !== null ) {
+    return localStorage.getItem('theme')
+  }
+  return window.matchMedia('(prefers-color-scheme: dark)').matches
+    ? 'dark'
+    : 'light'
+}
+
+const updateTheme = (theme) => {
+  localStorage.setItem('theme', theme)
+  if(theme === 'dark') {
+    document.documentElement.classList.add('darkMode')
+    $sun.style.display = 'block'
+    $moon.style.display = 'none'
+  }
+  if(theme === 'light') { 
+    document.documentElement.classList.remove('darkMode')
+    $sun.style.display = 'none'
+    $moon.style.display = 'block'
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  const theme = getTheme()
+  updateTheme(theme)
+});
